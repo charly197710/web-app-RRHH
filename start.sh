3f336723cade1e915a776444e1fa8ba05db493e9
charly197710 <charly197710@example.com>
2026-07-23 18:02:03 -0500

    Fix: Remove impossible himalaya dependency, fix build script to install binary, fix start.sh variable substitution

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p "$HOME/.config/himalaya"
-printf '[account]
-name = "default"
-imap_host = ""
-imap_port = 
-imap_username = ""
-imap_password = ""
-imap_auth_type = "password"
-' "$HIMALAYA_IMAP_HOST" "$HIMALAYA_IMAP_PORT" "$HIMALAYA_IMAP_USER" "$HIMALAYA_IMAP_PASS" > "$HOME/.config/himalaya/config.toml"
+printf '[account]\nname = "%s"\nimap_host = "%s"\nimap_port = "%s"\nimap_username = "%s"\nimap_password = "%s"\nimap_auth_type = "password"\n' "$HIMALAYA_IMAP_HOST" "$HIMALAYA_IMAP_PORT" "$HIMALAYA_IMAP_USER" "$HIMALAYA_IMAP_PASS" > "$HOME/.config/himalaya/config.toml"
 exec gunicorn -b 0.0.0.0:$PORT app:app
