7b09ac2d2bba0556ffe386b03b41f9e9671f8221
charly197710 <charly197710@example.com>
2026-07-23 12:35:15 -0500

    Fix start.sh to avoid heredoc expansion issues

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p "$HOME/.config/himalaya"
-cat > "$HOME/.config/himalaya/config.toml" <<EOF
-[[account]]
-name = "default"
-imap_host = "$HIMALAYA_IMAP_HOST"
-imap_port = $HIMALAYA_IMAP_PORT
-imap_username = "$HIMALAYA_IMAP_USER"
-imap_password = "$HIMALAYA_IMAP_PASS"
-imap_auth_type = "password"
-EOF
+printf '[account]\nname = "default"\nimap_host = "%s"\nimap_port = %s\nimap_username = "%s"\nimap_password = "%s"\nimap_auth_type = "password"\n' "$HIMALAYA_IMAP_HOST" "$HIMALAYA_IMAP_PORT" "$HIMALAYA_IMAP_USER" "$HIMALAYA_IMAP_PASS" > "$HOME/.config/himalaya/config.toml"
 exec gunicorn -b 0.0.0.0:$PORT app:app
